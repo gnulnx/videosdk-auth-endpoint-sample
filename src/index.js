d1ce1950f8e108c6152481325b79aabb49f5c87c
John  Furr <jfurr@johns-mbp-2.lan>
2024-06-25 06:33:23 -0400

    remove expressjs and prep for lambda deploy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import { KJUR } from 'jsrsasign'
-import { toStringArray } from './utils.js'
+import { KJUR } from 'jsrsasign';
+import { toStringArray } from './utils.js';
 import {
   inNumberArray,
   isBetween,
@@ -10,14 +7,7 @@ import {
   isRequired,
   matchesStringArray,
   validateRequest
-} from './validations.js'
-
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 4000
-
-app.use(express.json(), cors())
-app.options('*', cors())
+} from './validations.js';
 
 // Validations should match Zoom Video SDK's documentation:
 // https://developers.zoom.us/docs/video-sdk/auth/#payload
@@ -31,7 +21,7 @@ const validator = {
   cloudRecordingOption: inNumberArray([0, 1]),
   cloudRecordingElection: inNumberArray([0, 1]),
   audioCompatibleMode: inNumberArray([0, 1])
-}
+};
 
 const coerceRequestBody = (body) => ({
   ...body,
@@ -39,16 +29,19 @@ const coerceRequestBody = (body) => ({
     (acc, cur) => ({ ...acc, [cur]: typeof body[cur] === 'string' ? parseInt(body[cur]) : body[cur] }),
     {}
   )
-})
+});
 
-const joinGeoRegions = (geoRegions) => toStringArray(geoRegions)?.join(',')
+const joinGeoRegions = (geoRegions) => toStringArray(geoRegions)?.join(',');
 
-app.post('/', (req, res) => {
-  const requestBody = coerceRequestBody(req.body)
-  const validationErrors = validateRequest(requestBody, validator)
+export const handler = async (event) => {
+  const requestBody = coerceRequestBody(JSON.parse(event.body));
+  const validationErrors = validateRequest(requestBody, validator);
 
   if (validationErrors.length > 0) {
-    return res.status(400).json({ errors: validationErrors })
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ errors: validationErrors })
+    };
   }
 
   const {
@@ -61,11 +54,11 @@ app.post('/', (req, res) => {
     cloudRecordingOption,
     cloudRecordingElection,
     audioCompatibleMode
-  } = requestBody
+  } = requestBody;
 
-  const iat = Math.floor(Date.now() / 1000)
-  const exp = expirationSeconds ? iat + expirationSeconds : iat + 60 * 60 * 2
-  const oHeader = { alg: 'HS256', typ: 'JWT' }
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = expirationSeconds ? iat + expirationSeconds : iat + 60 * 60 * 2;
+  const oHeader = { alg: 'HS256', typ: 'JWT' };
 
   const oPayload = {
     app_key: process.env.ZOOM_VIDEO_SDK_KEY,
@@ -80,12 +73,14 @@ app.post('/', (req, res) => {
     cloud_recording_option: cloudRecordingOption,
     cloud_recording_election: cloudRecordingElection,
     audio_compatible_mode: audioCompatibleMode
-  }
+  };
 
-  const sHeader = JSON.stringify(oHeader)
-  const sPayload = JSON.stringify(oPayload)
-  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_VIDEO_SDK_SECRET)
-  return res.json({ signature: sdkJWT })
-})
+  const sHeader = JSON.stringify(oHeader);
+  const sPayload = JSON.stringify(oPayload);
+  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_VIDEO_SDK_SECRET);
 
-app.listen(port, () => console.log(`Zoom Video SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ signature: sdkJWT })
+  };
+};
